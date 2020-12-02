eaa75936bc9e2bdeba555c95518a2f9a79a24b92
wayneasimo <985340123@qq.com>
2020-12-02 22:51:00 +0800

    添加了main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: main.cpp
+	> Author:liyuhang 
+	> Mail:yuhangli@buaa.edu.cn 
+	> Created Time: 2020年12月02日 星期三 22时47分27秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "hello github\n" << endl;
+    return 0;
+}
